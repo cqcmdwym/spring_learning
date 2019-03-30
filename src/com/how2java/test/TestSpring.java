63b8a3c9a7295abab36d9d370a47e8b520e3b8d1
chenqi05 <chenqi05@juran.com.cn>
2019-03-30 16:14:51 +0800

    inject object

--- src/com/how2java/test/TestSpring.java
+++ src/com/how2java/test/TestSpring.java
@@ -3,7 +3,7 @@ package com.how2java.test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.how2java.pojo.Category;
+import com.how2java.pojo.Product;
 
 public class TestSpring {
 
@@ -11,9 +11,10 @@ public class TestSpring {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
                 new String[] { "applicationContext.xml" });
  
-        Category c = (Category) context.getBean("c");
-         
-        System.out.println(c.getName());
+		Product p = (Product) context.getBean("p");
+		 
+        System.out.println(p.getName());
+        System.out.println(p.getCategory().getName());
 	}
 
 }
