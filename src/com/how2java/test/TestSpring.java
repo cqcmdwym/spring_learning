f694c135f205d35469bffcd14508f25d15907a60
chenqi05 <chenqi05@juran.com.cn>
2019-04-01 18:41:22 +0800

    aop

--- src/com/how2java/test/TestSpring.java
+++ src/com/how2java/test/TestSpring.java
@@ -3,7 +3,7 @@ package com.how2java.test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.how2java.pojo.Product;
+import com.how2java.service.ProductService;
 
 public class TestSpring {
 
@@ -11,10 +11,8 @@ public class TestSpring {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
                 new String[] { "applicationContext.xml" });
  
-		Product p = (Product) context.getBean("p");
-		 
-        System.out.println(p.getName());
-        System.out.println(p.getCategory().getName());
+		ProductService s = (ProductService) context.getBean("s");
+        s.doSomeService();
 	}
 
 }
